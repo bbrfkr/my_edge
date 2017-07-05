28f2c66ecc69908df3d3e98aa0fa7f757982c130
root <root@deployedbysaito19216812711.localdomain>
2017-07-05 16:44:07 +0900

    openstack_keystone: first commit

--- Projects/openstack_keystone/docker-entrypoint.sh
+++ Projects/openstack_keystone/docker-entrypoint.sh
@@ -9,5 +9,67 @@ GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONE_DB
 EOF
 fi
 
+sed -i "s/KEYSTONE_DB_PASS/$KEYSTONE_DB_PASS/g" /etc/keystone/keystone.conf
+sed -i "s/MARIADB_HOSTNAME/$MARIADB_PORT_3306_TCP_ADDR/g" /etc/keystone/keystone.conf
+
+mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_PORT_3306_TCP_ADDR keystone -e "show tables;" \
+  | grep "Tables_in_keystone"
+if [ $? -ne 0 ] ; then
+  su -s /bin/sh -c "keystone-manage db_sync" keystone
+fi
+
+if [ ! -d /etc/keystone/fernet-keys ] ; then
+  keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+fi
+if [ ! -d /etc/keystone/credential-keys ] ; then
+  keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+fi
+
+MY_IP=`ip a | grep eth0 | grep inet | awk '{ print $2 }' | sed 's/\/.*//g'` 
+
+sed -i "s/\#\?ServerName .*/ServerName `uname -n`/g" /etc/httpd/conf/httpd.conf
+
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_AUTH_URL=http://$MY_IP:35357/v3
+export OS_IDENTITY_API_VERSION=3
+unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
+
+apachectl
+
+openstack project list | grep admin
+if [ $? -ne 0 ] ; then
+  keystone-manage bootstrap \
+    --bootstrap-password $ADMIN_PASS \
+    --bootstrap-username admin \
+    --bootstrap-project-name admin \
+    --bootstrap-role-name admin \
+    --bootstrap-service-name keystone \
+    --bootstrap-region-id $REGION_NAME \
+    --bootstrap-admin-url http://$MY_IP:35357 \
+    --bootstrap-public-url http://$MY_IP:5000 \
+    --bootstrap-internal-url http://$MY_IP:5000
+fi
+
+openstack project list | grep service
+if [ $? -ne 0 ] ; then
+  openstack project create --domain default --description "Service Project" service
+fi
+
+openstack role list | grep user
+if [ $? -ne 0 ] ; then
+  openstack role create user
+fi
+
+apachectl -k graceful-stop
+
+grep " admin_token_auth " /etc/keystone/keystone-paste.ini
+if [ $? -eq 0 ] ; then
+  sed -i 's/ admin_token_auth / /g'  /etc/keystone/keystone-paste.ini
+fi
+
 exec "$@"
 
--- Projects/openstack_keystone/spec/main_spec.rb
+++ Projects/openstack_keystone/spec/main_spec.rb
@@ -13,6 +13,9 @@ describe ("check necessary packages are installed") do
 end
 
 describe ("check keystone database is created") do
+  before :all do
+    sleep 60 
+  end
   describe command("mysql -uroot -ppassword -h#{ mariadb_ip } -e 'show databases;'") do
     its(:stdout) { should match /keystone/ }
   end
@@ -26,3 +29,58 @@ describe ("check keystone db user is created") do
     its(:stdout) { should match /GRANT ALL PRIVILEGES ON `keystone`\.\* TO 'keystone'@'%'/ }
   end
 end
+
+describe ("check keystone database is configureed") do
+  describe command("mysql -uroot -ppassword -h#{ mariadb_ip } keystone -e \"show tables;\"") do
+    its(:stdout) { should match /Tables_in_keystone/ }
+  end
+end
+
+describe ("check keystone fernet setup is completed") do
+  describe file("/etc/keystone/fernet-keys") do
+    it { should be_directory }
+  end
+end
+
+describe ("check keystone credential setup is completed") do
+  describe file("/etc/keystone/credential-keys") do
+    it { should be_directory }
+  end
+end
+
+describe ("check link wsgi-keystone.conf is created") do
+  describe file("/etc/httpd/conf.d/wsgi-keystone.conf") do 
+    it { should be_linked_to '/usr/share/keystone/wsgi-keystone.conf' }
+  end
+end
+
+describe ("check httpd process is enabled") do
+  describe process("httpd") do
+    it { should be_running }
+  end
+end
+
+describe ("check admin project is created") do
+  describe command("unset http_proxy HTTP_PROXY && openstack project list") do
+    its(:stdout) { should match /admin/ }
+  end
+end
+
+describe ("check service project is created") do
+  describe command("unset http_proxy HTTP_PROXY && openstack project list") do
+    its(:stdout) { should match /service/ }
+  end
+end
+
+describe ("check user role is created") do
+  describe command("unset http_proxy HTTP_PROXY && openstack role list") do
+    its(:stdout) { should match /user/ }
+  end
+end
+
+describe ("check admin_token entry doesn't have /etc/keystone/keystone-paste.ini") do
+  describe command("grep \" admin_token_auth \" /etc/keystone/keystone-paste.ini") do
+    its(:stdout) { should match /^$/ }
+  end
+end
+
